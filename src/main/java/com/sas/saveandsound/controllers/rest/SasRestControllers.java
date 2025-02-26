40b15bfbe63a9444763a96fef5d686cdd5f1ef22
Mpak-YT <antoshenko05@mail.ru>
2025-02-26 11:07:59 +0300

    what

--- src/main/java/com/sas/saveandsound/controllers/rest/SasRestControllers.java
+++ src/main/java/com/sas/saveandsound/controllers/rest/SasRestControllers.java
@@ -21,33 +21,33 @@ public class SasRestControllers {
 //        return new SoundEntity(query, "query");
 //    }
 
-    private final SoundEntity[] sounds = new SoundEntity[]{
-        new SoundEntity( "a1", "A1", "A2"),
-        new SoundEntity( "a1", "B2"),
-        new SoundEntity( "b1", "A1")
-    };
-
-    @GetMapping("/search")
-    public SoundEntity search(
-//        @RequestParam(value = "name", defaultValue = "") String name,
-//        @RequestParam(value = "creator", defaultValue = "") String[] creator,
-        @RequestParam(value = "query", defaultValue = "", required = false) String query) {
+//    private final SoundEntity[] sounds = new SoundEntity[]{
+//        new SoundEntity( "a1", "A1", "A2"),
+//        new SoundEntity( "a1", "B2"),
+//        new SoundEntity( "b1", "A1")
+//    };
+//
+//    @GetMapping("/search")
+//    public SoundEntity search(
+////        @RequestParam(value = "name", defaultValue = "") String name,
+////        @RequestParam(value = "creator", defaultValue = "") String[] creator,
+//        @RequestParam(value = "query", defaultValue = "", required = false) String query) {
+////        for (SoundEntity sound : sounds) {
+////            if ((sound.getName().equals(name) || name.isEmpty()) &&
+////                    (Arrays.equals(sound.getCreator(), creator) || creator.length == 0)) {
+////                return sound;
+////            }
+////        }
 //        for (SoundEntity sound : sounds) {
-//            if ((sound.getName().equals(name) || name.isEmpty()) &&
-//                    (Arrays.equals(sound.getCreator(), creator) || creator.length == 0)) {
+//            if ((sound.getName().equals(query) || query.isEmpty())) {
 //                return sound;
 //            }
 //        }
-        for (SoundEntity sound : sounds) {
-            if ((sound.getName().equals(query) || query.isEmpty())) {
-                return sound;
-            }
-        }
-        return null;
-    }
-
-    @GetMapping("/search/{userName}")
-    public String searchWithPathVariable(@PathVariable String userName) {
-        return "Hi, " + userName + "! Chang for test commit.";
-    }
+//        return null;
+//    }
+//
+//    @GetMapping("/search/{userName}")
+//    public String searchWithPathVariable(@PathVariable String userName) {
+//        return "Hi, " + userName + "! Chang for test commit.";
+//    }
 }
